6904e25a059b22df3b5b7fdde9ce629edeba817f
Dmitry Ageev <DAAgeev@yandex.ru>
2022-04-28 13:27:10 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        int amount = 1000;
+        int cost = 20;
+
+        int mille = amount / cost;
+
+        System.out.println("Количество миль: " + mille);
+
+        System.out.println("Bye!");
+    }
+}
